8ea5073854c93f76eb22f6b047c36c2b2cb4f461
Avyay-n <nishtala.avyay@gmail.com>
2025-07-16 19:24:56 +0530

    pagination done

--- IMDB_CLONE/src/components/Movies.jsx
+++ IMDB_CLONE/src/components/Movies.jsx
@@ -1,15 +1,26 @@
-import { useState } from "react";
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import MovieCard from "./MovieCard";
 import axios from "axios";
+import Pagination from "./Pagination";
 
 function Movies() {
   const [movies, setMovies] = useState([]);
+  const [pageNo, setPageNo] = useState(1);
+
+  const handlePrev = () => {
+    if (pageNo > 1) {
+      setPageNo(pageNo - 1);
+    }
+  };
+
+  const handleNext = () => {
+    setPageNo(pageNo + 1);
+  };
 
   useEffect(() => {
     axios
       .get(
-        "https://api.themoviedb.org/3/trending/movie/day?api_key=38539bcc78dbb94e7cd7313d2ddcb672&language=en-US&page=1"
+        `https://api.themoviedb.org/3/trending/movie/day?api_key=38539bcc78dbb94e7cd7313d2ddcb672&language=en-US&page=${pageNo}`
       )
       .then(function (res) {
         setMovies(res.data.results);
@@ -17,7 +28,7 @@ function Movies() {
       .catch(function (error) {
         console.error("Error fetching movies:", error);
       });
-  }, []);
+  }, [pageNo]);
 
   return (
     <div className="p-5">
@@ -26,9 +37,17 @@ function Movies() {
       </div>
       <div className="flex flex-row flex-wrap justify-around gap-8">
         {movies.map((movieObj) => {
-          return <MovieCard key={movieObj.id} poster_path={movieObj.poster_path} name={movieObj.original_title}/>;
+          return (
+            <MovieCard
+              key={movieObj.id}
+              poster_path={movieObj.poster_path}
+              name={movieObj.original_title}
+            />
+          );
         })}
       </div>
+
+      <Pagination pageNo={pageNo} handlePrev={handlePrev} handleNext={handleNext} />
     </div>
   );
 }
